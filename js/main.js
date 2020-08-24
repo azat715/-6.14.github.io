5c3f782167aa9b6000fc2423d18d6343c7aa4854
azat715 <azat715@gmail.com>
2020-08-24 23:22:33 +0500

    готово

--- js/main.js
+++ js/main.js
@@ -2,42 +2,89 @@ const numDivs = 36;
 const maxHits = 10;
 
 let hits = 0;
-let firstHitTime = 0;
 let missClick = 0;
 
+const btn1 = document.querySelector('.jbt-start');
+const btn2 = document.querySelector('.jbt-restart');
+
+const resWin = document.querySelector('.j-modal');
+
+times = {
+    firstHitTime: null,
+    start() {
+        this.firstHitTime = Date.now();
+    },
+    end() {
+        return Math.floor((Date.now() - this.firstHitTime) / 1000)
+    },
+}
 
 function round() {
-    console.log('запуск')
     let divSelector = getRandom();
     let targetRaw = document.getElementsByClassName(`j-slot${divSelector}`);
     let target = targetRaw['0'];
     target.classList.add('target');
-    console.log(target);
     target.textContent = hits;
 }
 
-const gameField = document.getElementsByClassName('j-game-field');
-gameField['0'].addEventListener('click', {
+const gameField = document.getElementsByClassName('j-game-field')['0'];
+gameField.addEventListener('click', {
     handleEvent: function (event) { 
-        console.log(event.explicitOriginalTarget.classList['2']);
         if (event.explicitOriginalTarget.classList['2'] == 'target') {
             hits++;
             event.explicitOriginalTarget.classList.remove('target')
+            event.explicitOriginalTarget.textContent = ""
             if (hits <= maxHits) {
                 round();
             } else {
-                console.log('конец')
+                endGame();
             }
         } else {
             missClick++; 
-            console.log('nenndnd')
+            event.explicitOriginalTarget.classList.add('miss');
+            setTimeout(() => {
+                event.explicitOriginalTarget.classList.remove('miss'); 
+            }, 1000);
         }
     }
 });
 
+function endGame() {
+    totalPlayedSeconds = times.end();
+    gameField.style['pointer-events'] = "none";
+    gameField.style['opacity'] = "0.4";
+    console.log(missClick);
+    console.log(totalPlayedSeconds);
+    resWin.style['opacity'] = "1";
+    resWin.style['visibility'] = "visible";
+    let resDiv = document.createElement('div');
+    resDiv.innerHTML = `<p>Вы выбрали 10 зеленых квадратов за ${totalPlayedSeconds} секунд <br />
+                        Количество ложных нажатий - ${missClick}</p>`;
+    resDiv.setAttribute('class', 'res')
+    resWin.appendChild(resDiv);
+}
+
+function init() {
+    btn1.addEventListener('click', () => {
+        gameField.style['display'] = "flex";
+        btn1.disabled = 'disabled';
+        times.start();
+        round();
+      });
+};
+
+btn2.addEventListener('click', () => {
+    resWin.style['opacity'] = "0";
+    resWin.style['visibility'] = "hidden";
+    gameField.style['pointer-events'] = "auto";
+    gameField.style['opacity'] = "1";
+    hits = 0;
+    missClick = 0;
+    times.start();
+    round();
+});
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    round();
-    console.log('запуск2')
-    // а тут нужно ждать когда загрузится весь код? вот эту часть можно убрать?
+    gameField.style['display'] = "none";
+    init();
   });
